4aa22f3d171f038d85841f9b3966960110e2df37
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2024-05-23 13:40:30 +0600

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+project = 'Sman'
+copyright = 'Copyright © 2024'
+author = 'sabbir28'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+# The short X.Y version
+version = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# Add any Sphinx extension module names here, as strings. They can be extensions
+# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
