aca190c15e8016110d2e7e0193b892ff538717fc
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2024-05-23 13:42:21 +0600

    Update conf.py
    update that for fix error

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'  # Comment out or remove this line
+html_theme = 'alabaster'  # Use the 'alabaster' theme instead
 html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
